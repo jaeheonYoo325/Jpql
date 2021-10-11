5035ac29cb9ce63e38e834b60515f33f98c79dc3
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-11 23:33:21 +0900

    2021.10.10 : JPQL 기본 문법과 쿼리 API.

---
+++ src/main/java/com/jpql/Jpql/JpqlMain.java
@@ -0,0 +1,48 @@
+package com.jpql.Jpql;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import com.jpql.Jpql.domain.Member;
+
+public class JpqlMain {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+			
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setAge(10);
+			em.persist(member);
+			
+			TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+			query.setParameter("username", "member1");
+			Member singleResult = query.getSingleResult();
+			System.out.println("singleResult = " + singleResult.getUsername());
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+		
+		emf.close();
+	}
+
+}
---
+++ src/main/java/com/jpql/Jpql/domain/Product.java
@@ -0,0 +1,49 @@
+package com.jpql.Jpql.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String gname;
+	private int price;
+	private int stockAmount;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGname() {
+		return gname;
+	}
+
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+}
