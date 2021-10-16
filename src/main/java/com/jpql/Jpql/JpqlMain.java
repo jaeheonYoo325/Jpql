f1de7cf85876aeeabb125dd255e4a2a63d5e11d7
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-16 22:31:47 +0900

    2021.10.16 : 벌크 연산.

--- src/main/java/com/jpql/Jpql/JpqlMain.java
+++ src/main/java/com/jpql/Jpql/JpqlMain.java
@@ -38,29 +38,30 @@ public class JpqlMain {
 			
 			Member member1 = new Member();
 			member1.setUsername("회원1");
+			member1.setAge(0);
 			member1.setTeam(teamA);			
 			em.persist(member1);
 			
 			Member member2 = new Member();
 			member2.setUsername("회원2");
+			member2.setAge(0);
 			member2.setTeam(teamA);			
 			em.persist(member2);
 			
 			Member member3 = new Member();
 			member3.setUsername("회원3");
+			member3.setAge(0);
 			member3.setTeam(teamB);			
 			em.persist(member3);
 			
-			em.flush();
+			int resultCount = em.createQuery("update Member m set m.age = '20'")
+				.executeUpdate();
+			
 			em.clear();
 			
-			List<Member> resultList = em.createNamedQuery("Member.findbyUsername", Member.class)
-					.setParameter("username", "회원1")
-					.getResultList();
+			Member findMember = em.find(Member.class, member1.getId());
+			System.out.println("findMember = " + findMember.getAge());
 			
-			for(Member member : resultList) {
-				System.out.println("member = " + member);
-			}
 			
 			tx.commit();
 		} catch (Exception e) {
