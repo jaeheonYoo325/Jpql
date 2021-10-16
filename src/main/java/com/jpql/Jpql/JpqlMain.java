3a2ba6d2f5a6b22d8f3c700566fefbbfd259a723
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-16 19:05:55 +0900

    2021.10.16 : 페치 조인1 - 기본.

--- src/main/java/com/jpql/Jpql/JpqlMain.java
+++ src/main/java/com/jpql/Jpql/JpqlMain.java
@@ -1,5 +1,6 @@
 package com.jpql.Jpql;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -27,27 +28,44 @@ public class JpqlMain {
 		
 		try {
 			
-			Team team = new Team();
-			team.setName("teamA");
-			em.persist(team);
+			Team teamA = new Team();
+			teamA.setName("팀A");
+			em.persist(teamA);
 			
-			Member member = new Member();
-			member.setUsername("관리자");
-			member.setAge(10);
-			member.setTeam(team);
+			Team teamB = new Team();
+			teamB.setName("팀B");
+			em.persist(teamB);
 			
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("회원1");
+			member1.setTeam(teamA);			
+			em.persist(member1);
 			
+			Member member2 = new Member();
+			member2.setUsername("회원2");
+			member2.setTeam(teamA);			
+			em.persist(member2);
+			
+			Member member3 = new Member();
+			member3.setUsername("회원3");
+			member3.setTeam(teamB);			
+			em.persist(member3);
 			
 			em.flush();
 			em.clear();
 			
-			String query = "select nullif(m.username, '관리자') as username from Member m ";
-			List<String> result = em.createQuery(query, String.class)
+			String query = "select t from Team t join fetch t.members";
+			List<Team> result = em.createQuery(query, Team.class)
 					.getResultList();
 			
-			for(String s : result) {
-				System.out.println("s = " + s);
+			System.out.println("result = " + result.size());
+			
+			for(Team team : result) {
+				System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+				
+				for( Member member :team.getMembers() ) {
+					System.out.println("-> member = " + member);
+				}
 			}
 			
 			tx.commit();
