140f791003189033c392c4ab9a276b538afb07d4
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-16 16:10:00 +0900

    2021.10.16 : 조건식(CASE 등등).

--- src/main/java/com/jpql/Jpql/JpqlMain.java
+++ src/main/java/com/jpql/Jpql/JpqlMain.java
@@ -32,7 +32,7 @@ public class JpqlMain {
 			em.persist(team);
 			
 			Member member = new Member();
-			member.setUsername("teamA");
+			member.setUsername("관리자");
 			member.setAge(10);
 			member.setTeam(team);
 			
@@ -42,17 +42,12 @@ public class JpqlMain {
 			em.flush();
 			em.clear();
 			
-			String query = "select m.username, 'HELLO', TRUE from Member m " +
-							"where m.type = :userType";
-			List<Object[]> result = em.createQuery(query)
-					.setParameter("userType", MemberType.ADMIN)
+			String query = "select nullif(m.username, '관리자') as username from Member m ";
+			List<String> result = em.createQuery(query, String.class)
 					.getResultList();
 			
-
-			for(Object[] objects : result) {
-				System.out.println("objects = " + objects[0]);
-				System.out.println("objects = " + objects[1]);
-				System.out.println("objects = " + objects[2]);
+			for(String s : result) {
+				System.out.println("s = " + s);
 			}
 			
 			tx.commit();
